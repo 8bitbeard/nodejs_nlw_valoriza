4d49ceb3724d58baeec1eb25155baaf7566f6453
Wilton Souza <willsouzafilho@gmail.com>
2021-08-01 15:48:46 -0300

    Adjusting the project repo

--- next_level_week_5/src/repositories/MessagesRepository.ts
+++
@@ -1,7 +0,0 @@
-import { Repository, EntityRepository } from "typeorm";
-import { Message } from "../entities/Message";
-
-@EntityRepository(Message)
-class MessagesRepository extends Repository<Message> {}
-
-export { MessagesRepository }
--- next_level_week_5/src/server.ts
+++
@@ -1,7 +0,0 @@
-import { http } from "./http";
-import "./websocket/client";
-import "./websocket/admin"
-
-let portNumber = 3000
-
-http.listen(portNumber, () => console.log(`Server is running on port ${portNumber}`));
